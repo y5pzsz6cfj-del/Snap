ce77c3bb1f5f5b802cb99e4d619969b4dcfccac1
y5pzsz6cfj-del <y5pzsz6cfj@privaterelay.appleid.com>
2026-05-23 03:23:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function hello() {
   alert("Hi 👍 JavaScript works");
 }
+function askCamera() {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+      alert("Camera permission granted 👍");
+    })
+    .catch(error => {
+      alert("Permission denied ❌");
+    });
+}
